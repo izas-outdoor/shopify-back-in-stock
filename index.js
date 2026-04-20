eeb038443bd78fcf13cff8c19d45584faa7663dd
izas-outdoor <61238167+izas-outdoor@users.noreply.github.com>
2026-04-20 15:07:03 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('@shopify/shopify-api/adapters/node');
 const express = require('express');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
